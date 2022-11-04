1c1ece072866af338f7555e9e5e904ff42aa218e
github-dns147 <rarden@ya.ru>
2022-11-04 21:01:59 +0300

    feat: add solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, 'files');
+const dirCopyPath = path.join(__dirname, 'files-copy');
 
+const eventError = err => {
+  if (err) {
+    throw err;
+  }
+};
+
+const copyFiles = file => {
+  const fileSrcPath = path.join(dirPath, file);
+  const fileDestPath = path.join(dirCopyPath, file);
+
+  fs.copyFile(fileSrcPath, fileDestPath, eventError);
+};
+
+fs.mkdir(dirCopyPath, err => {
+  if (err) {
+    fs.rm(dirCopyPath, { recursive: true }, eventError);
+    fs.readdir(dirPath, (err, files) => {
+      if (err) {
+        throw err;
+      }
+
+      files.forEach(copyFiles);
+    });
+  }
+});
+
+fs.rm(dirCopyPath, { recursive: true }, err => {
+  if (!err) {
+    fs.mkdir(dirCopyPath, eventError);
+    fs.readdir(dirPath, (err, files) => {
+      if (err) {
+        throw err;
+      }
+
+      files.forEach(copyFiles);
+    });
+  }
+});

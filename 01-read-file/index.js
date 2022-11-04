0fb85863a1d1e237676bbba077c9d03c41c67ebb
github-dns147 <rarden@ya.ru>
2022-11-04 19:43:44 +0300

    feat: add solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(filePath);
+
+const dataStream = data => {
+    const dataFile = Buffer.from(data);
+    //process.stdout.write(dataFile); // 1-ый способ вывода
+    console.log(dataFile.toString()); // 2-ой способ вывода
+} 
+
+stream.on('data', dataStream);
